6bcf461e93b25c0ebccf9caa914357e2fa717565
mc007 <asd28071977>
2015-01-23 13:46:58 +0100

    =

---
+++ main.js
@@ -0,0 +1,28 @@
+define([
+    "dojo/_base/kernel",
+    "dgrid/Editor",
+    "dgrid/Tree",
+    "dgrid/Selection",
+    "dgrid/OnDemandList",
+    "dgrid/OnDemandGrid",
+    "dgrid/Keyboard",
+    "dgrid/extensions/ColumnHider",
+    "dgrid/extensions/ColumnReorder",
+    "dgrid/extensions/ColumnResizer",
+    "dgrid/extensions/CompoundColumns",
+    "dgrid/extensions/Pagination",
+
+
+
+    "put-selector/put",
+
+
+    "dstore/Store",
+    "dstore/Memory",
+    "dstore/Tree",
+    "dstore/Cache",
+    "dstore/Trackable"
+
+], function(){
+    return dojo.dgrid;
+});
